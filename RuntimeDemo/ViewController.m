50d3fba1b9e87353bf00cf16d9323eeb7d2e4127
Abe_liu <Abe_liu@outlook.com>
2018-12-27 22:03:08 +0800

    提交消息转发机制的部分代码

--- RuntimeDemo/ViewController.m
+++ RuntimeDemo/ViewController.m
@@ -17,6 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //此处只是实现了类方法的调用，在进行实例方法的调用时，请注意target 的传参
+    Class class = NSClassFromString(@"Test");
+    SEL sel = NSSelectorFromString(@"addNumber");
+    NSMethodSignature *methodSign = [class methodSignatureForSelector:sel];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSign];
+    [invocation setTarget:class];
+    [invocation setSelector:sel];
+    [invocation invoke];
 }
 
 
